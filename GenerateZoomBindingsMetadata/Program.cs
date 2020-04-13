0a00f6f440f7e035b2bf1a2315ea45c9e97a4cf4
Olivier MATROT <omatrot@free.fr>
2020-04-13 09:57:20 +0200

    First commit

---
+++ GenerateZoomBindingsMetadata/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace GenerateZoomBindingsMetadata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (XmlReader reader = XmlReader.Create(@"/Users/omatrot/Projects/ZoomBindings/ZoomUtils/obj/Debug/api.xml"))
+            {
+                using (FileStream fs = new FileStream(@"/Users/omatrot/Projects/ZoomBindings/ZoomUtils/Transforms/Metadata.xml", FileMode.Create))
+                {
+                    var settings = new XmlWriterSettings();
+                    //settings.OmitXmlDeclaration = true;
+                    settings.Indent = true;
+                    //settings.NewLineOnAttributes = true;
+
+                    using (XmlWriter writer = XmlWriter.Create(fs, settings))
+                    {
+
+                        //writer.Formatting = Formatting.Indented;
+                        writer.WriteStartDocument();
+                        writer.WriteStartElement("metadata");
+                        foreach (XElement element in reader.ElementsNamed("package"))
+                        {
+                            // Grab the package name
+                            string packageName = element.Attribute("name").Value;
+                            // add this package to a remove node in Metadata.Xml
+                            if (!packageName.StartsWith("us.zoom"))
+                            {
+                                writer.WriteStartElement("remove-node");
+                                writer.WriteAttributeString("path", $@"/api/package[@name='{packageName}']");
+                                writer.WriteEndElement();
+                            }
+
+                        }
+                        writer.WriteEndElement();
+                        writer.Flush();
+                    }
+                }
+            }
+        }
+        
+    }
+}
---
+++ ZoomDemo/MainActivity.cs
@@ -0,0 +1,45 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using US.Zoom.Sdk;
+
+namespace ZoomDemo
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity, IZoomSDKInitializeListener
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            ZoomSDKInitParams initParams = new ZoomSDKInitParams();
+            initParams.AppKey = "x6yCTnMP3nWljshoJ6wPCagfTIbnF34AO0cc";
+            initParams.AppSecret = "u85nEugHkzG0RtXnPEfijtd7L3XnQZorVU2K";
+            initParams.Domain = "zoom.us";
+            initParams.VideoRawDataMemoryMode = ZoomSDKRawDataMemoryMode.ZoomSDKRawDataMemoryModeStack;
+
+            ZoomSDK.Instance.Initialize(this, this, initParams);
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        public void OnZoomAuthIdentityExpired()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void OnZoomSDKInitializeResult(int p0, int p1)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
